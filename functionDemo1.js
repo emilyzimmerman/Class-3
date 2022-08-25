cd8a6a70f87d7a892113ac0f16ef8c21b2b2cd85
emilyzimmerman <zimmermanemily121@gmail.com>
2022-08-25 17:41:33 -0500

    Completed Exercise 2

--- functionDemo1.js
+++ functionDemo1.js
@@ -4,3 +4,30 @@ function myString(name) {
 }; 
 
 console.log(myString("Emily"))
+
+
+
+const object1 = [{
+    a: 1, 
+    b: 2, 
+    c: 3, 
+}, 
+{
+    a: 'apples', 
+    b: 'coconuts', 
+    c: 'banannas',  
+
+}, 
+]; 
+
+
+
+function keysAndValues(obj){
+  let a = Object.keys(obj); 
+  let b = Object.values(obj);
+  let c = [a, b];
+
+  return c; 
+ }
+
+ console.log(keysAndValues(object1)); 
